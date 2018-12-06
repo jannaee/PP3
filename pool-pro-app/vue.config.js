37b64a1d0819fe6e318bfd8602590bb8ad63746d
Jannaee Sick <jannaee@gmail.com>
2018-12-06 18:46:37 -0500

    feat: My node_modules folder disappeared! Re-installed it

--- pool-pro-app/vue.config.js
+++ pool-pro-app/vue.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/css/main.scss")]
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, './src/css/main.scss')]
     }
   }
-};
+}
