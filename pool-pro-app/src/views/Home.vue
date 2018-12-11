9e1b01af980b48cd97f16ba4be72964a2b3bed25
Jannaee <jannaee@gmail.com>
2018-12-11 11:46:58 -0500

    feat: begining the styles for dealer cards:

--- pool-pro-app/src/views/Home.vue
+++ pool-pro-app/src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <main>
-    <ul>
+    <ul class="dealer-cards">
       <li v-for='(card , index) in cards' :key='index'>
         <!-- <h2>{{ cards.location }}</h2>
         <p>{{cards.zipcode}}</p> -->
         <p>{{ cards.dealers[0].data.name }}</p>
+
       </li>
     </ul>
   </main>
@@ -19,3 +20,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .dealer-cards {
+    li {
+      list-style-type: none;
+      border: 1px solid #000000;
+      display: inline-flex;
+      flex-direction: row;
+      padding: 30px;
+      margin: 20px;
+      width: 100px;
+      border-radius: 2px;
+      box-shadow: 2px 2px 2px rgba(0,0,0, .2);
+
+    }
+
+  }
+</style>
