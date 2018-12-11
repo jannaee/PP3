4fb6299b735305ea249e193229bb13eaa0ff66f7
Jannaee Sick <jannaee@gmail.com>
2018-12-11 12:03:57 -0500

    chore: styling dealer cards

--- pool-pro-app/src/views/Home.vue
+++ pool-pro-app/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
-    <ul class="dealer-cards">
-      <li v-for='(card , index) in cards' :key='index'>
+    <ul class="container">
+      <li class="dealer-cards" v-for='(card , index) in cards' :key='index'>
         <!-- <h2>{{ cards.location }}</h2>
         <p>{{cards.zipcode}}</p> -->
         <p>{{ cards.dealers[0].data.name }}</p>
@@ -22,19 +22,20 @@ export default {
 </script>
 
 <style lang="scss">
+  .container {
+    display: inline-flex;
+    flex-flow: row;
+    justify-content: center;
+
+  }
   .dealer-cards {
-    li {
       list-style-type: none;
       border: 1px solid #000000;
-      display: inline-flex;
-      flex-direction: row;
       padding: 30px;
-      margin: 20px;
-      width: 100px;
-      border-radius: 2px;
+      height: 40vh;
+      margin: 5px;
+      width: 15vw;
+      border-radius: 6px;
       box-shadow: 2px 2px 2px rgba(0,0,0, .2);
-
-    }
-
   }
 </style>
